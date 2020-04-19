68a114963bf1b66a16c944d2cdce7dac68551001
Your Name <you@example.com>
2020-04-19 16:46:58 +0800

    初始化

---
+++ src/pages/aaa/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <div>
+        <h2>如何使用git pull，特此创建以便了解</h2>
+        <h3>---第一次提交代码---</h3>
+      </div>
+    );
+  }
+}
+
+export default index;
