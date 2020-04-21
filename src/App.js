7f3b315a24873cfbade5525d9afa1034a188433c
Your Name <you@example.com>
2020-04-21 12:46:33 +0800

    luyao

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Index from './pages/aaa'
-
+import Indext from './pages/bbb'
 function App() {
   return (
     <div>
       <h1>如何使用GIT</h1>
       <Index/>
+      <hr/>
+      <Indext/>
     </div>
     
   );
--- src/pages/aaa/index.js
+++ src/pages/aaa/index.js
@@ -6,6 +6,7 @@ class index extends Component {
       <div>
         <h2>如何使用git pull，特此创建以便了解</h2>
         <h3>---第一次提交代码---</h3>
+        <h3>---我打代码给刘宇航看---</h3>
       </div>
     );
   }
