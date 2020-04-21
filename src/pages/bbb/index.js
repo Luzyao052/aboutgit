56226e1e10c43dec13310b9e97c68df0a4cd880e
河兮 <>
2020-04-21 11:43:56 +0800

    刘宇航

---
+++ src/pages/bbb/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class index extends Component {
+  render() {
+    return (
+      <div>
+        <h2>啦啦啦</h2>
+        <h3>---第二次提交代码---</h3>
+      </div>
+    );
+  }
+}
+
+export default index;
